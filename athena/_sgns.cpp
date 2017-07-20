33bdb4aa2bdaedf7883fe0038537327b88a8c971
Chandler May <cjmay4754@gmail.com>
2017-07-19 20:07:10 -0400

    remove SGNSModel

--- athena/_sgns.cpp
+++ athena/_sgns.cpp
@@ -18,91 +18,28 @@ using namespace std;
 
 
 //
-// SGNSModel
-//
-
-
-void SGNSModel::serialize(ostream& stream) const {
-  Serializer<WordContextFactorization>::serialize(*factorization, stream);
-  Serializer<SamplingStrategy>::serialize(*neg_sampling_strategy, stream);
-  Serializer<LanguageModel>::serialize(*language_model, stream);
-  Serializer<SGD>::serialize(*sgd, stream);
-  Serializer<ContextStrategy>::serialize(*ctx_strategy, stream);
-  Serializer<SGNSTokenLearner>::serialize(*token_learner, stream);
-  Serializer<SGNSSentenceLearner>::serialize(*sentence_learner, stream);
-  Serializer<SubsamplingSGNSSentenceLearner>::serialize(
-    *subsampling_sentence_learner, stream);
-}
-
-shared_ptr<SGNSModel> SGNSModel::deserialize(istream& stream) {
-  auto factorization_(
-      Serializer<WordContextFactorization>::deserialize(stream));
-  auto neg_sampling_strategy_(
-      Serializer<SamplingStrategy>::deserialize(stream));
-  auto language_model_(
-      Serializer<LanguageModel>::deserialize(stream));
-  auto sgd_(Serializer<SGD>::deserialize(stream));
-  auto ctx_strategy_(Serializer<ContextStrategy>::deserialize(stream));
-  auto token_learner_(Serializer<SGNSTokenLearner>::deserialize(stream));
-  auto sentence_learner_(Serializer<SGNSSentenceLearner>::deserialize(stream));
-  auto subsampling_sentence_learner_(
-      Serializer<SubsamplingSGNSSentenceLearner>::deserialize(stream));
-  auto model(make_shared<SGNSModel>(
-    factorization_,
-    neg_sampling_strategy_,
-    language_model_,
-    sgd_,
-    ctx_strategy_,
-    token_learner_,
-    sentence_learner_,
-    subsampling_sentence_learner_
-  ));
-  token_learner_->set_model(model);
-  sentence_learner_->set_model(model);
-  subsampling_sentence_learner_->set_model(model);
-  return model;
-}
-
-bool SGNSModel::equals(const SGNSModel& other) const {
-  return
-    factorization->equals(*(other.factorization)) &&
-    neg_sampling_strategy->equals(*(other.neg_sampling_strategy)) &&
-    language_model->equals(*(other.language_model)) &&
-    sgd->equals(*(other.sgd)) &&
-    ctx_strategy->equals(*(other.ctx_strategy)) &&
-    token_learner->equals(*(other.token_learner)) &&
-    sentence_learner->equals(*(other.sentence_learner)) &&
-    subsampling_sentence_learner->equals(
-      *(other.subsampling_sentence_learner));
-}
-
-
-//
 // SGNSTokenLearner
 //
 
 
 void SGNSTokenLearner::reset_word(long word_idx) {
-  auto model = _model.lock();
-  model->sgd->reset(word_idx);
+  sgd->reset(word_idx);
   sample_centered_uniform_vector(
-    model->factorization->get_embedding_dim(),
-    model->factorization->get_word_embedding(word_idx)
+    factorization->get_embedding_dim(),
+    factorization->get_word_embedding(word_idx)
   );
-  memset(model->factorization->get_context_embedding(word_idx), 0,
-         model->factorization->get_embedding_dim() * sizeof(float));
+  memset(factorization->get_context_embedding(word_idx), 0,
+         factorization->get_embedding_dim() * sizeof(float));
 }
 
 long SGNSTokenLearner::find_context_nearest_neighbor_idx(size_t left_context,
                                                     size_t right_context,
                                                     const long *word_ids) {
-  auto model = _model.lock();
-
   long best_candidate_word_idx = -1;
   float best_score = -numeric_limits<float>::max();
 
   for (size_t candidate_word_idx = 0;
-       candidate_word_idx < model->language_model->size();
+       candidate_word_idx < language_model->size();
        ++candidate_word_idx) {
     // should we try to take a MAP estimate here?
     float log_prob_ctx_given_candidate = 0;
@@ -113,9 +50,9 @@ long SGNSTokenLearner::find_context_nearest_neighbor_idx(size_t left_context,
         if (context_word_idx >= 0) {
           log_prob_ctx_given_candidate += fast_sigmoid(
             cblas_sdot(
-              model->factorization->get_embedding_dim(),
-              model->factorization->get_word_embedding(candidate_word_idx), 1,
-              model->factorization->get_context_embedding(context_word_idx), 1
+              factorization->get_embedding_dim(),
+              factorization->get_word_embedding(candidate_word_idx), 1,
+              factorization->get_context_embedding(context_word_idx), 1
             )
           );
         }
@@ -133,31 +70,27 @@ long SGNSTokenLearner::find_context_nearest_neighbor_idx(size_t left_context,
 
 float SGNSTokenLearner::compute_similarity(size_t word1_idx,
                                            size_t word2_idx) const {
-  auto model = _model.lock();
-
   return cblas_sdot(
-    model->factorization->get_embedding_dim(),
-    model->factorization->get_word_embedding(word1_idx), 1,
-    model->factorization->get_word_embedding(word2_idx), 1
+    factorization->get_embedding_dim(),
+    factorization->get_word_embedding(word1_idx), 1,
+    factorization->get_word_embedding(word2_idx), 1
   ) / (
     cblas_snrm2(
-      model->factorization->get_embedding_dim(),
-      model->factorization->get_word_embedding(word1_idx), 1
+      factorization->get_embedding_dim(),
+      factorization->get_word_embedding(word1_idx), 1
     ) * cblas_snrm2(
-      model->factorization->get_embedding_dim(),
-      model->factorization->get_word_embedding(word2_idx), 1
+      factorization->get_embedding_dim(),
+      factorization->get_word_embedding(word2_idx), 1
     )
   );
 }
 
 long SGNSTokenLearner::find_nearest_neighbor_idx(size_t word_idx) {
-  auto model = _model.lock();
-
   long best_candidate_word_idx = -1;
   float best_score = -numeric_limits<float>::max();
 
   for (size_t candidate_word_idx = 0;
-       candidate_word_idx < model->language_model->size();
+       candidate_word_idx < language_model->size();
        ++candidate_word_idx) {
     if (candidate_word_idx != word_idx) {
       const float score = compute_similarity(candidate_word_idx, word_idx);
@@ -174,11 +107,10 @@ long SGNSTokenLearner::find_nearest_neighbor_idx(size_t word_idx) {
 float SGNSTokenLearner::compute_gradient_coeff(long target_word_idx,
                                            long context_word_idx,
                                            bool negative_sample) const {
-  auto model = _model.lock();
   return (negative_sample ? 0 : 1) - fast_sigmoid(cblas_sdot(
-    model->factorization->get_embedding_dim(),
-    model->factorization->get_word_embedding(target_word_idx), 1,
-    model->factorization->get_context_embedding(context_word_idx), 1
+    factorization->get_embedding_dim(),
+    factorization->get_word_embedding(target_word_idx), 1,
+    factorization->get_context_embedding(context_word_idx), 1
   ));
 }
 
@@ -195,12 +127,6 @@ bool SGNSTokenLearner::context_contains_oov(const long* ctx_word_ids,
 void SGNSTokenLearner::token_train(size_t target_word_idx,
                                      size_t context_word_idx,
                                      size_t neg_samples) {
-  auto model = _model.lock();
-  auto factorization(model->factorization);
-  auto sgd(model->sgd);
-  auto neg_sampling_strategy(model->neg_sampling_strategy);
-  auto language_model(model->language_model);
-
   // initialize target (input) word gradient
   AlignedVector target_word_gradient(
     factorization->get_embedding_dim());
@@ -257,14 +183,34 @@ void SGNSTokenLearner::token_train(size_t target_word_idx,
   );
 }
 
-void SGNSTokenLearner::serialize(ostream& stream) const { }
+void SGNSTokenLearner::serialize(ostream& stream) const {
+  Serializer<WordContextFactorization>::serialize(*factorization, stream);
+  Serializer<SamplingStrategy>::serialize(*neg_sampling_strategy, stream);
+  Serializer<LanguageModel>::serialize(*language_model, stream);
+  Serializer<SGD>::serialize(*sgd, stream);
+}
 
 shared_ptr<SGNSTokenLearner> SGNSTokenLearner::deserialize(istream& stream) {
-  return make_shared<SGNSTokenLearner>();
+  auto factorization_(
+      Serializer<WordContextFactorization>::deserialize(stream));
+  auto neg_sampling_strategy_(
+      Serializer<SamplingStrategy>::deserialize(stream));
+  auto language_model_(
+      Serializer<LanguageModel>::deserialize(stream));
+  auto sgd_(Serializer<SGD>::deserialize(stream));
+  return make_shared<SGNSTokenLearner>(
+    factorization_,
+    neg_sampling_strategy_,
+    language_model_,
+    sgd_
+  );
 }
 
 bool SGNSTokenLearner::equals(const SGNSTokenLearner& other) const {
-  return true;
+  return factorization->equals(*(other.factorization)) &&
+    neg_sampling_strategy->equals(*(other.neg_sampling_strategy)) &&
+    language_model->equals(*(other.language_model)) &&
+    sgd->equals(*(other.sgd));
 }
 
 
@@ -274,21 +220,17 @@ bool SGNSTokenLearner::equals(const SGNSTokenLearner& other) const {
 
 
 void SGNSSentenceLearner::increment(const string& word) {
-  auto model = _model.lock();
-
   const pair<long,string> ejectee =
-    model->language_model->increment(word);
+    token_learner->language_model->increment(word);
   const long ejectee_idx = ejectee.first;
   if (ejectee_idx >= 0) {
-    model->token_learner->reset_word(ejectee_idx);
+    token_learner->reset_word(ejectee_idx);
   }
-  model->neg_sampling_strategy->step(*(model->language_model),
-                                      model->language_model->lookup(word));
+  token_learner->neg_sampling_strategy->step(*(token_learner->language_model),
+                                      token_learner->language_model->lookup(word));
 }
 
 void SGNSSentenceLearner::sentence_train(const vector<string>& words) {
-  auto model = _model.lock();
-
   // (optionally) add all words in sentence to language model
   if (_propagate_retained)
     for (auto it = words.begin(); it != words.end(); ++it)
@@ -300,7 +242,7 @@ void SGNSSentenceLearner::sentence_train(const vector<string>& words) {
   word_ids.reserve(words.size());
   size_t num_oov = 0;
   for (auto it = words.begin(); it != words.end(); ++it) {
-    long word_id = model->language_model->lookup(*it);
+    long word_id = token_learner->language_model->lookup(*it);
     if (word_id >= 0) {
       word_ids.push_back(word_id);
     } else {
@@ -313,7 +255,7 @@ void SGNSSentenceLearner::sentence_train(const vector<string>& words) {
   for (size_t target_word_pos = 0; target_word_pos < word_ids.size();
        ++target_word_pos) {
     // compute context size
-    const pair<size_t,size_t> ctx_size = model->ctx_strategy->size(
+    const pair<size_t,size_t> ctx_size = ctx_strategy->size(
       target_word_pos, (word_ids.size() - 1) - target_word_pos
     );
     const size_t left_ctx = ctx_size.first;
@@ -329,24 +271,30 @@ void SGNSSentenceLearner::sentence_train(const vector<string>& words) {
       for (size_t i = ctx_start; i < ctx_end; ++i) {
         // for all context (output) words...
         if (i != target_word_pos) {
-          model->token_learner->token_train(word_ids[target_word_pos],
+          token_learner->token_train(word_ids[target_word_pos],
                                             word_ids[i], _neg_samples);
         }
       }
-      model->sgd->step(word_ids[target_word_pos]);
+      token_learner->sgd->step(word_ids[target_word_pos]);
     }
   }
 }
 
 void SGNSSentenceLearner::serialize(ostream& stream) const {
+  Serializer<SGNSTokenLearner>::serialize(*token_learner, stream);
+  Serializer<ContextStrategy>::serialize(*ctx_strategy, stream);
   Serializer<size_t>::serialize(_neg_samples, stream);
   Serializer<bool>::serialize(_propagate_retained, stream);
 }
 
 shared_ptr<SGNSSentenceLearner> SGNSSentenceLearner::deserialize(istream& stream) {
+  auto token_learner_(Serializer<SGNSTokenLearner>::deserialize(stream));
+  auto ctx_strategy_(Serializer<ContextStrategy>::deserialize(stream));
   auto neg_samples(*Serializer<size_t>::deserialize(stream));
   auto propagate_retained(*Serializer<bool>::deserialize(stream));
   return make_shared<SGNSSentenceLearner>(
+    token_learner_,
+    ctx_strategy_,
     neg_samples,
     propagate_retained
   );
@@ -354,6 +302,8 @@ shared_ptr<SGNSSentenceLearner> SGNSSentenceLearner::deserialize(istream& stream
 
 bool SGNSSentenceLearner::equals(const SGNSSentenceLearner& other) const {
   return
+    token_learner->equals(*(other.token_learner)) &&
+    ctx_strategy->equals(*(other.ctx_strategy)) &&
     _neg_samples == other._neg_samples &&
     _propagate_retained == other._propagate_retained;
 }
@@ -366,29 +316,30 @@ bool SGNSSentenceLearner::equals(const SGNSSentenceLearner& other) const {
 
 void SubsamplingSGNSSentenceLearner::sentence_train(const vector<string>&
                                                       words) {
-  auto model = _model.lock();
-
   vector<string> subsampled_words;
   for (auto it = words.begin(); it != words.end(); ++it) {
     const string& word(*it);
-    const long word_id = model->language_model->lookup(word);
-    if (word_id < 0 || model->language_model->subsample(word_id)) {
+    const long word_id = sentence_learner->token_learner->language_model->lookup(word);
+    if (word_id < 0 || sentence_learner->token_learner->language_model->subsample(word_id)) {
       subsampled_words.push_back(word);
     } else if (_propagate_discarded) {
-      model->sentence_learner->increment(word);
+      sentence_learner->increment(word);
     }
   }
-  model->sentence_learner->sentence_train(subsampled_words);
+  sentence_learner->sentence_train(subsampled_words);
 }
 
 void SubsamplingSGNSSentenceLearner::serialize(ostream& stream) const {
+  Serializer<SGNSSentenceLearner>::serialize(*sentence_learner, stream);
   Serializer<bool>::serialize(_propagate_discarded, stream);
 }
 
 shared_ptr<SubsamplingSGNSSentenceLearner>
     SubsamplingSGNSSentenceLearner::deserialize(istream& stream) {
+  auto sentence_learner_(Serializer<SGNSSentenceLearner>::deserialize(stream));
   auto propagate_discarded(*Serializer<bool>::deserialize(stream));
   return make_shared<SubsamplingSGNSSentenceLearner>(
+    sentence_learner_,
     propagate_discarded
   );
 }
@@ -396,5 +347,6 @@ shared_ptr<SubsamplingSGNSSentenceLearner>
 bool SubsamplingSGNSSentenceLearner::equals(
     const SubsamplingSGNSSentenceLearner& other) const {
   return
+    sentence_learner->equals(*(other.sentence_learner)) &&
     _propagate_discarded == other._propagate_discarded;
 }
--- athena/_sgns.h
+++ athena/_sgns.h
@@ -13,7 +13,6 @@
 class SGNSTokenLearner;
 class SGNSSentenceLearner;
 class SubsamplingSGNSSentenceLearner;
-struct SGNSModel;
 
 
 // Core SGNS implementation.  At training time takes a single input-output
@@ -22,10 +21,21 @@ struct SGNSModel;
 // instead).
 
 class SGNSTokenLearner {
-  std::weak_ptr<SGNSModel> _model;
-
   public:
-    SGNSTokenLearner(): _model() { }
+    std::shared_ptr<WordContextFactorization> factorization;
+    std::shared_ptr<SamplingStrategy> neg_sampling_strategy;
+    std::shared_ptr<LanguageModel> language_model;
+    std::shared_ptr<SGD> sgd;
+
+    SGNSTokenLearner(
+        std::shared_ptr<WordContextFactorization> factorization_,
+        std::shared_ptr<SamplingStrategy> neg_sampling_strategy_,
+        std::shared_ptr<LanguageModel> language_model_,
+        std::shared_ptr<SGD> sgd_):
+            factorization(factorization_),
+            neg_sampling_strategy(neg_sampling_strategy_),
+            language_model(language_model_),
+            sgd(sgd_) { }
     virtual void reset_word(long word_idx);
     virtual void token_train(size_t target_word_idx, size_t context_word_idx,
                              size_t neg_samples);
@@ -39,9 +49,6 @@ class SGNSTokenLearner {
                                                    const long *word_ids);
     virtual bool context_contains_oov(const long* ctx_word_ids,
                                       size_t ctx_size) const;
-    virtual void set_model(std::shared_ptr<SGNSModel> model) {
-      _model = model;
-    }
     virtual ~SGNSTokenLearner() { }
 
     virtual bool equals(const SGNSTokenLearner& other) const;
@@ -61,20 +68,24 @@ class SGNSTokenLearner {
 // over the words within each context.
 
 class SGNSSentenceLearner {
-  std::weak_ptr<SGNSModel> _model;
-  size_t _neg_samples;
-  bool _propagate_retained;
+  public:
+    std::shared_ptr<SGNSTokenLearner> token_learner;
+    std::shared_ptr<ContextStrategy> ctx_strategy;
+
+  private:
+    size_t _neg_samples;
+    bool _propagate_retained;
 
   public:
-    SGNSSentenceLearner(size_t neg_samples, bool propagate_retained):
-      _model(),
+    SGNSSentenceLearner(std::shared_ptr<SGNSTokenLearner> token_learner_,
+        std::shared_ptr<ContextStrategy> ctx_strategy_,
+        size_t neg_samples, bool propagate_retained):
+      token_learner(token_learner_),
+      ctx_strategy(ctx_strategy_),
       _neg_samples(neg_samples),
       _propagate_retained(propagate_retained) { }
     virtual void increment(const std::string& word);
     virtual void sentence_train(const std::vector<std::string>& words);
-    virtual void set_model(std::shared_ptr<SGNSModel> model) {
-      _model = model;
-    }
     virtual ~SGNSSentenceLearner() { }
 
     virtual bool equals(const SGNSSentenceLearner& other) const;
@@ -90,16 +101,19 @@ class SGNSSentenceLearner {
 // (as in word2vec) before training.
 
 class SubsamplingSGNSSentenceLearner {
-  std::weak_ptr<SGNSModel> _model;
-  bool _propagate_discarded;
+  public:
+    std::shared_ptr<SGNSSentenceLearner> sentence_learner;
+
+  private:
+    bool _propagate_discarded;
 
   public:
-    SubsamplingSGNSSentenceLearner(bool propagate_discarded):
-      _model(), _propagate_discarded(propagate_discarded) { }
+    SubsamplingSGNSSentenceLearner(
+        std::shared_ptr<SGNSSentenceLearner> sentence_learner_,
+        bool propagate_discarded):
+      sentence_learner(sentence_learner_),
+      _propagate_discarded(propagate_discarded) { }
     virtual void sentence_train(const std::vector<std::string>& words);
-    virtual void set_model(std::shared_ptr<SGNSModel> model) {
-      _model = model;
-    }
     virtual ~SubsamplingSGNSSentenceLearner() { }
 
     virtual bool equals(const SubsamplingSGNSSentenceLearner& other) const;
@@ -108,41 +122,6 @@ class SubsamplingSGNSSentenceLearner {
 
   private:
     SubsamplingSGNSSentenceLearner(const SubsamplingSGNSSentenceLearner& s);
-};
-
-
-struct SGNSModel {
-  std::shared_ptr<WordContextFactorization> factorization;
-  std::shared_ptr<SamplingStrategy> neg_sampling_strategy;
-  std::shared_ptr<LanguageModel> language_model;
-  std::shared_ptr<SGD> sgd;
-  std::shared_ptr<ContextStrategy> ctx_strategy;
-  std::shared_ptr<SGNSTokenLearner> token_learner;
-  std::shared_ptr<SGNSSentenceLearner> sentence_learner;
-  std::shared_ptr<SubsamplingSGNSSentenceLearner> subsampling_sentence_learner;
-
-  SGNSModel(
-      std::shared_ptr<WordContextFactorization> factorization_,
-      std::shared_ptr<SamplingStrategy> neg_sampling_strategy_,
-      std::shared_ptr<LanguageModel> language_model_,
-      std::shared_ptr<SGD> sgd_,
-      std::shared_ptr<ContextStrategy> ctx_strategy_,
-      std::shared_ptr<SGNSTokenLearner> token_learner_,
-      std::shared_ptr<SGNSSentenceLearner> sentence_learner_,
-      std::shared_ptr<SubsamplingSGNSSentenceLearner>
-        subsampling_sentence_learner_):
-          factorization(factorization_),
-          neg_sampling_strategy(neg_sampling_strategy_),
-          language_model(language_model_),
-          sgd(sgd_),
-          ctx_strategy(ctx_strategy_),
-          token_learner(token_learner_),
-          sentence_learner(sentence_learner_),
-          subsampling_sentence_learner(subsampling_sentence_learner_) { }
-
-  virtual void serialize(std::ostream& stream) const;
-  static std::shared_ptr<SGNSModel> deserialize(std::istream& stream);
-  virtual bool equals(const SGNSModel& other) const;
 };
 
 
